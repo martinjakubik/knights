e513020c365ecd1e53f0d9160d586996c74f9106
martin jakubik <martin.jakubik@sap.com>
2024-02-18 22:51:38 +0100

    painted black and white pawns

--- scripts/export-cards.sh
+++ scripts/export-cards.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+. ~/.bashrc
 if [[ ! -e build ]] ; then
     mkdir build
 fi
@@ -9,5 +9,6 @@ for layerId in {1..6}; do
     label=$(xmllint --xpath "string(/*[local-name() = 'svg']/*[local-name() = 'g'][@*[local-name() = 'id'] = 'layer${layerId}']/@*[local-name() = 'label'])"  resources/chesspieces.svg)
 
     # exports two versions of the drawing scene
-    inkscape resources/chesspieces.svg -i layer${layerId} -j -C --export-png=build/piece-${label}.scene.forhtml.png
+    inkscape resources/chesspieces.svg -i layer${layerId} -j -C --export-png=build/piece-${label}.b.forhtml.png
+    magick build/piece-${label}.b.forhtml.png -alpha deactivate -negate build/piece-${label}.w.forhtml.png
 done

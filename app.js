1fee639f4406160dfb1365d629c1587cabb8303e
martin jakubik <martin.jakubik@sap.com>
2024-02-18 19:31:34 +0100

    setup ids for pieces

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-let aChessboard = [];
+let oChessboard = {};
 const NUM_RANKS = 8;
 const NUM_FILES = 8;
 const aFiles = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-for (nRankIndex = 0; nRankIndex < NUM_RANKS; nRankIndex++) {
-    let aRank = [];
-    for (nFileIndex = 0; nFileIndex < NUM_FILES; nFileIndex++) {
-        aRank.push(undefined);
+CHESSBOARD_START = {
+    'a1': 'wr',
+    'b1': 'wn',
+    'c1': 'wb',
+    'd1': 'wq',
+    'e1': 'wk',
+    'f1': 'wb',
+    'g1': 'wk',
+    'h1': 'wr',
+    'a2': 'wp',
+    'b2': 'wp',
+    'c2': 'wp',
+    'd2': 'wp',
+    'e2': 'wp',
+    'f2': 'wp',
+    'g2': 'wp',
+    'h2': 'wp',
+    'a8': 'br',
+    'b8': 'bn',
+    'c8': 'bb',
+    'd8': 'bq',
+    'e8': 'bk',
+    'f8': 'bb',
+    'g8': 'bk',
+    'h8': 'br',
+    'a7': 'bp',
+    'b7': 'bp',
+    'c7': 'bp',
+    'd7': 'bp',
+    'e7': 'bp',
+    'f7': 'bp',
+    'g7': 'bp',
+    'h7': 'bp'
+}
+
+const clearChessboard = function () {
+    let sSquareKey = '';
+    for (nRankIndex = 0; nRankIndex < NUM_RANKS; nRankIndex++) {
+        let nRank = nRankIndex + 1;
+        for (nFileIndex = 0; nFileIndex < NUM_FILES; nFileIndex++) {
+            let sFile = aFiles[nFileIndex];
+            sSquareKey = `${sFile}${nRank}`;
+            oChessboard[sSquareKey] = '';
+        }
     }
-    aChessboard.push(aRank);
+}
+
+const setupChessboard = function (oSavedChessboard) {
+    clearChessboard();
+    Object.keys(oSavedChessboard).forEach(sSquareKey => {
+        oChessboard[sSquareKey] = oSavedChessboard[sSquareKey];
+    })
 }
 
 const getMaximumBoardDisplaySize = function () {
@@ -22,7 +68,7 @@ const getHighLowForSquare = function (nRankIndex, nFileIndex) {
     return (nRankIndex + nFileIndex) % 2 == 0 ? true : false;
 }
 
-const drawChessboard = function (aChessboard) {
+const drawChessboard = function (oChessboard) {
     let oChessboardDiv = document.createElement('div');
     oChessboardDiv.id = 'chessboard';
     const nMaximumBoardWidth = getMaximumBoardDisplaySize();
@@ -43,8 +89,10 @@ const drawChessboard = function (aChessboard) {
             bHigh = getHighLowForSquare(nRankIndex, nFileIndex);
             oDiv.classList.add(bHigh ? 'high' : 'low');
             oChessboardDiv.appendChild(oDiv);
+            let oPiece = oChessboard[`${sFile}${nRank}`];
         };
     }
 }
 
-drawChessboard();
+setupChessboard(CHESSBOARD_START);
+drawChessboard(oChessboard);

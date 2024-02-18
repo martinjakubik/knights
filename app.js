4b35c8df51019bd0b1ef81b1ce3d7a47da78a817
martin jakubik <martin.jakubik@sap.com>
2024-02-18 15:48:08 +0100

    nice board on phone

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 let aChessboard = [];
+const NUM_RANKS = 8;
+const NUM_FILES = 8;
 const aFiles = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-for (nRankIndex = 0; nRankIndex < 8; nRankIndex++) {
+for (nRankIndex = 0; nRankIndex < NUM_RANKS; nRankIndex++) {
     let aRank = [];
-    for (nFileIndex = 0; nFileIndex < 8; nFileIndex++) {
+    for (nFileIndex = 0; nFileIndex < NUM_FILES; nFileIndex++) {
         aRank.push(undefined);
     }
     aChessboard.push(aRank);
 }
 
+const getMaximumBoardDisplaySize = function () {
+    const nViewportWidth = document.documentElement.clientWidth;
+    const nViewportHeight = document.documentElement.clientHeight;
+    const nMaximumSize = nViewportWidth <= nViewportHeight ? nViewportWidth : nViewportHeight;
+    return nMaximumSize;
+}
+
+const getHighLowForSquare = function (nRankIndex, nFileIndex) {
+    return (nRankIndex + nFileIndex) % 2 == 0 ? true : false;
+}
+
 const drawChessboard = function (aChessboard) {
     let oChessboardDiv = document.createElement('div');
     oChessboardDiv.id = 'chessboard';
+    const nMaximumBoardWidth = getMaximumBoardDisplaySize();
+    oChessboardDiv.style.width = nMaximumBoardWidth;
+    oChessboardDiv.style.height = nMaximumBoardWidth;
+    const nSquareSize = nMaximumBoardWidth / NUM_RANKS;
     document.body.appendChild(oChessboardDiv);
-    for (nRankIndex = 0; nRankIndex < 8; nRankIndex++) {
+    let bHigh = true;
+    for (nRankIndex = 0; nRankIndex < NUM_RANKS; nRankIndex++) {
         let nRank = nRankIndex + 1;
-        for (nFileIndex = 0; nFileIndex < 8; nFileIndex++) {
+        for (nFileIndex = 0; nFileIndex < NUM_FILES; nFileIndex++) {
             let oDiv = document.createElement('div');
             let sFile = aFiles[nFileIndex];
             oDiv.classList.add('square');
             oDiv.id = `${sFile}${nRank}`;
+            oDiv.style.width = nSquareSize;
+            oDiv.style.height = nSquareSize;
+            bHigh = getHighLowForSquare(nRankIndex, nFileIndex);
+            oDiv.classList.add(bHigh ? 'high' : 'low');
             oChessboardDiv.appendChild(oDiv);
         };
     }
